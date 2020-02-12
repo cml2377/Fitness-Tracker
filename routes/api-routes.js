edcd251b5dc6fd2a28e4af2abd4e19cfb9bc9f05
Crystal Ly <crystal.m.ly@gmail.com>
2020-02-11 18:06:38 -0600

    added CRUD routes/methods to api-routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -8,7 +8,7 @@ const db = require("../models");
 // Routes 
 //===================================================
 module.exports = function (app) {
-    // Get/Read route for getting all exercises
+    // Get/Read route for getting all workouts.
     app.get("/api/workouts", function (req, res) {
         // getting it out of Workout.js in models.
         db.Workout.findAll({}).then(function (dbWorkout) {
@@ -17,7 +17,7 @@ module.exports = function (app) {
             res.json(err);
         })
     })
-    // Get single workout by id
+    // Get single workout by id.
     app.get("/api/workouts/:id", function (req, res) {
         db.Workout.findOne({
             where: {
@@ -29,6 +29,38 @@ module.exports = function (app) {
             res.json(err);
         })
     })
-    // Post/Create route for creating a new workout session
-
+    // Post/Create a new workout in the database.
+    app.post("/api/workouts", function (req, res) {
+        db.Workout.create(req.body).then(function (dbWorkout) {
+            res.json(dbWorkout);
+        }).catch(err => {
+            res.json(err);
+        })
+    })
+    // Update a workout in the database by id.
+    app.put("/api/workouts", function (req, res) {
+        db.Workout.update(
+            req.body,
+            {
+                where: {
+                    id: req.body.id
+                }
+            }).then(function (dbWorkout) {
+                res.json(dbWorkout);
+            }).catch(err => {
+                res.json(err);
+            })
+    })
+    // Delete a workout in the database.
+    app.delete("/api/workouts/:id", function (req, res) {
+        db.Workout.destroy({
+            where: {
+                id: req.params.id
+            }
+        }).then(function (dbWorkout) {
+            res.json(dbWorkout);
+        }).catch(err => {
+            res.json(err);
+        })
+    })
 }
